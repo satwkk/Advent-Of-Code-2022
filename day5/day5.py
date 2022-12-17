7218e5daa80d51c691a857f82d49128e32b4f189
Satweek Tripathy <smoxics@gmail.com>
2022-12-18 02:02:29 +0530

    day 5 part 2

--- day5/day5.py
+++ day5/day5.py
@@ -1,32 +1,54 @@
 from typing import List
 
-with open('input.txt', 'r') as input_file:
+with open('E:\\Advent of Code\\2022\\day5\\input.txt', 'r') as input_file:
     lines = input_file.read()
     
-# Part 1
-
-def peek(s: List[int]) -> str:
-    return s[-1]
-
 n_crates = len(lines.splitlines()[0]) // 4 + 1
-stacks = [[] for i in range(n_crates)]
 
 data = lines.split('\n\n')
 stack_diag = data[0]
 operations = data[1]
-for i, items in enumerate(stack_diag.splitlines()[:-1][::-1]):
-    for idx, char in enumerate(items):
-        if char == '[':
-            stacks[idx // 4].append(items[idx+1])
 
-for operation in operations.splitlines():
-    n = int(operation.split(' ')[1])
-    f = int(operation.split(' ')[3]) - 1
-    t = int(operation.split(' ')[5]) - 1
-    for i in range(n):
-        stacks[t].append(stacks[f].pop())
+# Part 1
+def part1():
+    stacks = [[] for i in range(n_crates)]
+    for i, items in enumerate(stack_diag.splitlines()[:-1][::-1]):
+        for idx, char in enumerate(items):
+            if char == '[':
+                stacks[idx // 4].append(items[idx+1])
 
+    for operation in operations.splitlines():
+        n = int(operation.split(' ')[1])
+        f = int(operation.split(' ')[3]) - 1
+        t = int(operation.split(' ')[5]) - 1
+        for _ in range(n):
+            stacks[t].append(stacks[f].pop())
+    
+    return ''.join([stack[-1] for stack in stacks])
 
-part1 = ''
-part1 += ''.join([stack[-1] for stack in stacks])
-print(f'Part1: {part1}')
+print(f'Part1: {part1()}')
+
+# Part 2
+def part2():
+    stacks = [[] for i in range(n_crates)]
+    for i, items in enumerate(stack_diag.splitlines()[:-1][::-1]):
+        for idx, char in enumerate(items):
+            if char == '[':
+                stacks[idx // 4].append(items[idx+1])
+
+    for operation in operations.splitlines():
+        n = int(operation.split(' ')[1])
+        f = int(operation.split(' ')[3]) - 1
+        t = int(operation.split(' ')[5]) - 1
+        
+        temp_list = list()
+        
+        for _ in range(n):
+            temp_list.append(stacks[f].pop())
+            
+        for _ in range(len(temp_list)):
+            stacks[t].append(temp_list.pop())
+    
+    return ''.join([stack[-1] for stack in stacks])
+    
+print(f"Part2: {part2()}")
