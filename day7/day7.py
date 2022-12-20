6043cb5862fe8ecd8d7e411cd6438f74c1113223
Satweek Tripathy <smoxics@gmail.com>
2022-12-20 10:32:23 +0530

    day 7 part 2

--- day7/day7.py
+++ day7/day7.py
@@ -3,7 +3,7 @@ from typing import List
 
 # QUESTION: https://adventofcode.com/2022/day/7
 
-with open(sys.argv[1], 'r') as input_file:
+with open("E:\\advent of code\\2022\\day7\\input.txt", 'r') as input_file:
     lines = input_file.read().strip()
 lines = lines.splitlines()
 
@@ -24,13 +24,22 @@ class Dir:
         self.back: Dir = None
         self.files: List[File] = list()
         self.dirs: List[Dir] = list()
-        self.size = 0
         
     def __str__(self):
         return self.name
 
     def __repr__(self):
         return self.name
+    
+    def get_size(self):
+        child_file_sizes = []
+        for dirs in self.dirs:
+            child_file_sizes.append(dirs.get_size())
+        
+        if child_file_sizes:
+            return sum(child_file_sizes) + sum([f.size for f in self.files])
+        else:
+            return sum([f.size for f in self.files])
     
     
 curr_dir: Dir = None
@@ -45,8 +54,11 @@ for idx, line in enumerate(commands):
             arg = line.split(' ')[-1]
             if arg == '..':
                 # change current directory one back
-                if curr_dir.back:
-                    curr_dir = curr_dir.back
+                curr_dir = curr_dir.back
+                
+            elif arg == '/':
+                curr_dir = root_dir
+                    
             else:
                 # change current directory to argument
                 for d_idx, d in enumerate(curr_dir.dirs):
@@ -76,29 +88,33 @@ for idx, line in enumerate(commands):
 '''
 Part1
 
-Kinda ugly algorithm but ok
-
 TODO: We can improve traversing by storing every files, subdirectories and subfiles inside a dictionary, rather than
 storing them in two separate lists. 
 
 '''
-part1_answers = []
-def initialize_size(dir: Dir):
-    temp = dir
-    total = 0
-    
-    total += sum([f.size for f in temp.files])
-    
-    for dirs in temp.dirs:
-        total += initialize_size(dirs)
+
+# Part 1
+valid_dir_sizes = list()
+def get_valid_dirs(dir: Dir):
+    if dir.get_size() <= 100000:
+        valid_dir_sizes.append(dir.get_size())
         
-    dir.size = total
+    for dirs in dir.dirs:
+        get_valid_dirs(dirs)
+
+get_valid_dirs(root_dir)
+print(f"Part1: {sum(valid_dir_sizes)}")
     
-    if dir.size <= 100000:
-        part1_answers.append(dir.size)
+# Part2
+limit = 30000000 - (70000000 - root_dir.get_size())
+valid_candids = list()
+def get_valid_dirs_to_delete(dir: Dir):
+    if dir.get_size() >= limit:
+        valid_candids.append(dir.get_size())
     
-    return total
-    
-initialize_size(root_dir)
-print(f"Part1: {sum(part1_answers)}")
-        
+    for dirs in dir.dirs:
+        get_valid_dirs_to_delete(dirs)
+
+get_valid_dirs_to_delete(root_dir)
+print(f'Part2: {min(valid_candids)}')
+
